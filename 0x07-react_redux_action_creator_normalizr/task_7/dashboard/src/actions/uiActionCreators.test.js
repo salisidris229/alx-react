c54fbd373430c69b8e212f4a62edd65cedba07f3
salisidris229 <salisuidris229@gmail.com>
2023-11-23 14:55:22 -0800

    Async Action Creators

--- 0x07-react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.test.js
+++ 0x07-react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.test.js
@@ -1,6 +1,11 @@
 import { LOGIN, LOGOUT, DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER } from "./uiActionTypes";
-
 import { login, logout, hideNotificationDrawer, displayNotificationDrawer } from "./uiActionCreators";
+import configureStore from "redux-mock-store";
+import thunk from "redux-thunk";
+import fetchMock from "fetch-mock";
+
+const middleWares = [thunk];
+const mockStore = configureStore(middleWares);
 
 describe("tests for UI notification action creators", () => {
   it("should create proper action for login", () => {
@@ -28,4 +33,46 @@ describe("tests for UI notification action creators", () => {
       type: HIDE_NOTIFICATION_DRAWER,
     });
   });
+
+  // it('should pass LOGIN and LOGIN_SUCCESS to the store if API returns the right response', () => {
+  // 	fetchMock.get('http://localhost:8564/login-success.json', {
+  // 		first_name: 'Johann',
+  // 		last_name: 'Salva',
+  // 		email: 'johann.salva@alxafrica.nz',
+  // 		profile_picture: 'http://placehold.it/32x32',
+  // 	});
+
+  // 	const store = mockStore({});
+  // 	const email = 'johann.salva@alxafrica.nz';
+  // 	const password = 'password';
+
+  // 	return store.dispatch(
+  // 		loginRequest(email, password).then(() => {
+  // 			const actions = store.getActions();
+  // 			console.log(actions);
+  // 			expect(actions[0]).toEqual(loginSuccess());
+  // 			expect(actions[1]).toEqual(login());
+  // 		})
+  // 	);
+  // 	fetchMock.restore();
+  // });
+
+  // it('should pass LOGIN and LOGIN_FAILURE if API query fails', () => {
+  // 	fetchMock.get('http://localhost:8564/login-success.json', 400);
+
+  // 	const store = mockStore({});
+  // 	const email = 'johann.salva@alxafrica.nz';
+  // 	const password = 'password';
+
+  // 	return store.dispatch(
+  // 		loginRequest(email, password).then(() => {
+  // 			const actions = store.getActions();
+  // 			console.log(actions);
+  // 			expect(actions[0]).toEqual(loginFailure());
+  // 			expect(actions[1]).toEqual(login());
+  // 		})
+  // 	);
+
+  // 	fetchMock.restore();
+  // });
 });
