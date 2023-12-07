1f9a985bf1d4b8379a12b2c9408e0d1cd85c590d
salisidris229 <salisuidris229@gmail.com>
2023-12-06 16:01:12 -0800

    applied middleware to store, connected loginRequest to App component, connected user state to Footer componenet, connected Logout action creator to Header component, added login to uiReducer and updated tests

--- 0x09-react_redux_connectors_and_providers/task_2/src/Header/Header.js
+++ 0x09-react_redux_connectors_and_providers/task_2/src/Header/Header.js
@@ -1,55 +1,87 @@
-import React from 'react';
-import { StyleSheet, css } from 'aphrodite';
-import holberton_logo from '../assets/holberton_logo.jpeg';
-import { AppContext } from '../App/AppContext';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { logout } from "../actions/uiActionCreators";
+import PropTypes from "prop-types";
+import { StyleSheet, css } from "aphrodite";
+import AppContext from "../App/AppContext";
+import holberton_logo from "../assets/holberton_logo.jpg";
 
-class Header extends React.Component {
-	render() {
-		const data = this.context;
-		return (
-			<React.Fragment>
-				<div className={css(styles.header)}>
-					<img src={holberton_logo} className={css(styles.logo)} alt='logo' />
-					<h1 className={css(styles.title)}>School dashboard</h1>
-				</div>
-				{data.user.isLoggedIn && (
-					<div className={css(styles.greeting)} id='logoutSection'>
-						Welcome {data.user.email}{' '}
-						<a className={css(styles.logout)} onClick={data.logOut}>
-							(logout)
-						</a>
-					</div>
-				)}
-			</React.Fragment>
-		);
-	}
+export class Header extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { user, logout } = this.props;
+
+    return (
+      <div className={css(styles.header)}>
+        <img src={holberton_logo} className={css(styles.headerImg)} />
+        <h1>School dashboard</h1>
+
+        {user && (
+          <p id="logoutSection" className={css(styles.logoutSection)}>
+            Welcome <b>{`${user.email} `}</b>
+            <span onClick={logout} className={css(styles.logoutSectionSpan)}>
+              (logout)
+            </span>
+          </p>
+        )}
+      </div>
+    );
+  }
 }
+
+const cssVars = {
+  mainColor: "#e01d3f",
+};
+
+const styles = StyleSheet.create({
+  header: {
+    display: "flex",
+    alignItems: "center",
+    color: cssVars.mainColor,
+    fontSize: "20px",
+  },
+
+  headerImg: {
+    width: "200px",
+  },
+  logoutSection: {
+    color: "black",
+    position: "absolute",
+    right: 0,
+    paddingRight: "20px",
+    alignSelf: "flex-end",
+  },
+  logoutSectionSpan: {
+    fontStyle: "italic",
+    cursor: "pointer",
+  },
+});
 
 Header.contextType = AppContext;
 
-const styles = StyleSheet.create({
-	header: {
-		display: 'flex',
-		alignItems: 'center',
-		fontSize: 'calc(10px + 2vmin)',
-		color: 'white',
-		'@media (max-width: 900px)': {
-			justifyContent: 'center',
-		},
-	},
-	title: {
-		color: 'red',
-	},
-	logo: {
-		height: '100px',
-		width: '100px',
-	},
-	greeting: {
-		marginTop: '1rem',
-	},
-	logout: {
-		cursor: 'pointer',
-	},
-});
+Header.defaultProps = {
+  user: null,
+  logout: () => {},
+};
 
-export default Header;
+Header.propTypes = {
+  user: PropTypes.object,
+  logout: PropTypes.func,
+};
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.get("user"),
+  };
+};
+
+const mapDispatchToProps = {
+  logout,
+};
+
+// export default Header;
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
--- 0x09-react_redux_connectors_and_providers/task_2/src/Header/Header.test.js
+++ 0x09-react_redux_connectors_and_providers/task_2/src/Header/Header.test.js
@@ -1,86 +1,43 @@
-/**
- * @jest-environment jsdom
- */
-import React from 'react';
-import { StyleSheetTestUtils } from 'aphrodite';
-import Header from './Header';
 import { shallow, mount } from 'enzyme';
-import { AppContext, defaultUser, defaultLogout } from '../App/AppContext';
+import React from 'react';
+import { Header } from './Header';
+import { StyleSheetTestUtils } from 'aphrodite';
+import AppContext, { user, logOut } from '../App/AppContext';
 
-beforeEach(() => {
-	StyleSheetTestUtils.suppressStyleInjection();
-});
+const USER = { email: 'michael@mifflin.com', password: 'pippity' };
 
-afterEach(() => {
-	StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
-});
-
-describe('Tests for Header component', () => {
-	it('renders without crashing', () => {
-		const wrapper = shallow(
-			<AppContext.Provider>
-				<Header />
-			</AppContext.Provider>
-		);
-
-		expect(wrapper.exists()).toBe(true);
+describe('<Header />', () => {
+	beforeAll(() => {
+		StyleSheetTestUtils.suppressStyleInjection();
+	});
+	afterAll(() => {
+		StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
 	});
 
-	it('renders img and h1 tags', () => {
-		const wrapper = mount(<Header />);
-
-		expect(wrapper.exists('img')).toBe(true);
-		expect(wrapper.exists('h1')).toBe(true);
+	it('Header renders without crashing', () => {
+		const wrapper = shallow(<Header />);
+		expect(wrapper.exists()).toEqual(true);
 	});
-});
-
-describe('context tests', () => {
-	it('mounts with default context value and not create logoutSection', () => {
-		const wrapper = mount(
-			<AppContext.Provider value={{ user: defaultUser, logout: defaultLogout }}>
-				<Header />
-			</AppContext.Provider>
-		);
-
-		expect(wrapper.find('img')).toHaveLength(1);
-		expect(wrapper.find('h1')).toHaveLength(1);
-		expect(wrapper.find('#logoutSection').exists()).toBe(false);
+	it('verifies that the components render img', () => {
+		const wrapper = shallow(<Header user={USER} />);
+		wrapper.update();
+		expect(wrapper.find('div img')).toHaveLength(1);
+	});
+	it('verifies that the components render h1', () => {
+		const wrapper = shallow(<Header user={USER} />);
+		wrapper.update();
+		expect(wrapper.find('div h1')).toHaveLength(1);
 	});
 
-	it('should mount with defined user and create logoutSection', () => {
-		const dummy = {
-			email: 'fred@gmail.com',
-			password: 'pass123',
-			isLoggedIn: true,
-		};
-		const wrapper = mount(
-			<AppContext.Provider value={{ user: dummy, logout: defaultLogout }}>
-				<Header />
-			</AppContext.Provider>
-		);
+	it('mounts the Header component with a default context value. The logoutSection is not created', () => {
+		const wrapper = shallow(<Header />);
 
-		expect(wrapper.find('img')).toHaveLength(1);
-		expect(wrapper.find('h1')).toHaveLength(1);
-		expect(wrapper.find('#logoutSection').exists()).toBe(true);
+		expect(wrapper.find('#logoutSection')).toHaveLength(0);
 	});
 
-	it('should mount with defined user and call logOut when link is clicked', () => {
-		const testData = {
-			user: {
-				email: 'fred@gmail.com',
-				password: 'pass123',
-				isLoggedIn: true,
-			},
-			logOut: () => {},
-		};
-		const spy = jest.spyOn(testData, 'logOut');
-		const wrapper = mount(
-			<AppContext.Provider value={testData}>
-				<Header />
-			</AppContext.Provider>
-		);
-		wrapper.find('#logoutSection a').simulate('click');
-		expect(spy).toHaveBeenCalled();
-		spy.mockRestore();
+	it('mounts the Header component with a user defined (isLoggedIn is true and an email is set). The logoutSection is created', () => {
+		const wrapper = shallow(<Header user={USER} />);
+
+		expect(wrapper.find('#logoutSection')).toHaveLength(1);
 	});
 });
