7289264d5f6828e103e932ab280041a3472f3852
salisidris229 <salisuidris229@gmail.com>
2023-12-06 16:41:58 -0800

    combined all reducers and initial states into single root reducer and root state, updated all points in application where state data is consumed and updated tests

--- 0x09-react_redux_connectors_and_providers/task_4/dashboard/src/App/App.test.js
+++ 0x09-react_redux_connectors_and_providers/task_4/dashboard/src/App/App.test.js
@@ -1,96 +1,100 @@
-import { shallow, mount } from "enzyme";
-import React from "react";
-import { App, listNotificationsInitialState, mapStateToProps } from "./App";
-import { StyleSheetTestUtils } from "aphrodite";
-import AppContext, { user, logOut } from "./AppContext";
+import { shallow, mount } from 'enzyme';
+import React from 'react';
+import { App, listNotificationsInitialState, mapStateToProps } from './App';
+import { StyleSheetTestUtils } from 'aphrodite';
+import AppContext, { user, logOut } from './AppContext';
 
-import { fromJS } from "immutable";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
-import uiReducer, { initialState } from "../reducers/uiReducer";
+import { fromJS } from 'immutable';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import uiReducer, { initialState } from '../reducers/uiReducer';
 
 const store = createStore(uiReducer, initialState);
 
-describe("<App />", () => {
-  beforeAll(() => {
-    StyleSheetTestUtils.suppressStyleInjection();
-  });
-  afterAll(() => {
-    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
-  });
+describe('<App />', () => {
+	beforeAll(() => {
+		StyleSheetTestUtils.suppressStyleInjection();
+	});
+	afterAll(() => {
+		StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+	});
 
-  it("App renders without crashing", () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.exists()).toEqual(true);
-  });
-  it("should contain the Notifications component", () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find("Notifications")).toHaveLength(1);
-  });
+	it('App renders without crashing', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.exists()).toEqual(true);
+	});
+	it('should contain the Notifications component', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.find('Notifications')).toHaveLength(1);
+	});
 
-  it("should contain the Login component", () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find("Login")).toHaveLength(1);
-  });
+	it('should contain the Login component', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.find('Login')).toHaveLength(1);
+	});
 
-  it("CourseList is not displayed with isLoggedIn false by default", () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find("CourseList")).toHaveLength(0);
-  });
+	it('CourseList is not displayed with isLoggedIn false by default', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.find('CourseList')).toHaveLength(0);
+	});
 
-  it("isLoggedIn is true", () => {
-    const wrapper = shallow(<App isLoggedIn={true} />);
+	it('isLoggedIn is true', () => {
+		const wrapper = shallow(<App isLoggedIn={true} />);
 
-    expect(wrapper.find("Login")).toHaveLength(0);
-    expect(wrapper.find("CourseList")).toHaveLength(1);
-  });
+		expect(wrapper.find('Login')).toHaveLength(0);
+		expect(wrapper.find('CourseList')).toHaveLength(1);
+	});
 
-  it("verify that markNotificationAsRead works as intended", () => {
-    const wrapper = shallow(<App />);
+	it('verify that markNotificationAsRead works as intended', () => {
+		const wrapper = shallow(<App />);
 
-    const instance = wrapper.instance();
+		const instance = wrapper.instance();
 
-    expect(wrapper.state().listNotifications).toEqual(
-      listNotificationsInitialState
-    );
+		expect(wrapper.state().listNotifications).toEqual(
+			listNotificationsInitialState
+		);
 
-    instance.markNotificationAsRead(4);
+		instance.markNotificationAsRead(4);
 
-    expect(wrapper.state().listNotifications).toEqual(
-      listNotificationsInitialState
-    );
+		expect(wrapper.state().listNotifications).toEqual(
+			listNotificationsInitialState
+		);
 
-    instance.markNotificationAsRead(3);
+		instance.markNotificationAsRead(3);
 
-    expect(wrapper.state().listNotifications).toEqual(
-      listNotificationsInitialState.slice(0, 2)
-    );
+		expect(wrapper.state().listNotifications).toEqual(
+			listNotificationsInitialState.slice(0, 2)
+		);
 
-    instance.markNotificationAsRead(1);
+		instance.markNotificationAsRead(1);
 
-    expect(wrapper.state().listNotifications).toEqual(
-      listNotificationsInitialState.slice(1, 2)
-    );
-  });
+		expect(wrapper.state().listNotifications).toEqual(
+			listNotificationsInitialState.slice(1, 2)
+		);
+	});
 });
 
-describe("App Redux", () => {
-  it("mapStateToProps returns the right object from user Login", () => {
-    let state = fromJS({
-      isUserLoggedIn: true,
-    });
+describe('App Redux', () => {
+	it('mapStateToProps returns the right object from user Login', () => {
+		let state = {
+			ui: fromJS({
+				isUserLoggedIn: true,
+			}),
+		};
 
-    const result = mapStateToProps(state);
+		const result = mapStateToProps(state);
 
-    expect(result).toEqual({ isLoggedIn: true });
-  });
-  it("mapStateToProps returns the right object from display Drawer", () => {
-    let state = fromJS({
-      isNotificationDrawerVisible: true,
-    });
+		expect(result).toEqual({ isLoggedIn: true });
+	});
+	it('mapStateToProps returns the right object from display Drawer', () => {
+		let state = {
+			ui: fromJS({
+				isNotificationDrawerVisible: true,
+			}),
+		};
 
-    const result = mapStateToProps(state);
+		const result = mapStateToProps(state);
 
-    expect(result).toEqual({ displayDrawer: true });
-  });
+		expect(result).toEqual({ displayDrawer: true });
+	});
 });
